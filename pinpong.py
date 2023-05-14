5fc40967aef547aa869587de57ebf98bef873a84
Nik <77504477+Lisa-Red-moon@users.noreply.github.com>
2023-05-14 11:28:07 +0300

    Update pinpong.py

--- pinpong.py
+++ pinpong.py
@@ -18,16 +18,35 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
     def move1(self):
-        if key_pressed[K_w] and self.rect.y <=300:
+        if key_pressed[K_s] and self.rect.y <=300:
             self.rect.y += self.speed
-        if key_pressed[K_s] and self.rect.y >=5:
+        if key_pressed[K_w] and self.rect.y >=5:
             self.rect.y -= self.speed
     def move2(self):
-        if key_pressed[K_UP] and self.rect.y <=300:
+        if key_pressed[K_DOWN] and self.rect.y <=300:
             self.rect.y += self.speed
-        if key_pressed[K_DOWN] and self.rect.y >=5:
+        if key_pressed[K_UP] and self.rect.y >=5:
             self.rect.y -= self.speed
 
+speed_x = 3
+speed_y = 3
+
+finish = False
+
+class Ball(GameSprite):
+    def move_ball(self):
+        global speed_x, speed_y, finish
+        if finish != True:
+            self.rect.x += speed_x
+            self.rect.y += speed_y
+        if ball.rect.y > 450 or self.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x > 650 or self.rect.x < 0:
+            finish = True
+        if sprite.collide_rect(stick1, ball) or sprite.collide_rect(stick2, ball):
+            speed_x *= -1
+
+ball = Ball('eyeball.png', 325, 250, 50, 50, 0)
 background = transform.scale(image.load('forest.jpg'),(700,500))
 stick1 = GameSprite('sprite.png', 0, 0, 25, 200, 7)
 stick2 = GameSprite('sprite.png', 675, 0, 25, 200, 7)
@@ -36,6 +55,7 @@ game = True
 finish = False
 
 while game:
+
     key_pressed = key.get_pressed()
     for i in event.get():
 
@@ -45,6 +65,9 @@ while game:
     window.blit( background , (0,0))
     stick1.reset()
     stick2.reset()
+
+    ball.reset()
+    ball.move_ball()
 
     stick1.move1()
     stick2.move2()
